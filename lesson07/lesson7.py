bf435840038289fb57a46cdd7580b3df7bc2a4f6
Vanessa <vanessaphung07@gmail.com>
2025-02-19 15:21:32 -0500

    cat

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,14 @@
 # Lesson 7
+import random
+dc = int(input("enter number 1 through 20 representing the diffculty class "))
+if (1 <= dc <= 20):
+    d20 = random.randrange(1,20)
+    print (f"you rolled a {d20}")
+    if d20 > dc:
+        print ("you rolled a number higher than the dc. You suceed! ")
+    elif d20 < dc:
+        print ("you rolled a number lower than the dc. You fail :( ")
+    elif d20 == dc: 
+         print ("you rolled the same number as the dc. It's a tie. ")
+else: 
+    print ("not a number 1-20, try again")
