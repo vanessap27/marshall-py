96b28dc9edd0c5df89e1cbd2c16b490b9a82fd82
Vanessa <vanessaphung07@gmail.com>
2025-02-21 12:59:41 -0500

    cat

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,15 @@
 # Lesson 11
+user_input = input("enter the x and y coordinates of the point: ")
+coords = user_input.split(" ")
+x = int(coords[0])
+y = int(coords[1])
+if x > 0:
+    if y > 1:
+        print(f"the point ({x}, {y}) is in quadrant one ")
+    else: 
+        print(f"the point ({x}, {y}) is in quadrant four ")
+if x < 0:
+    if y > 1:
+        print(f"the point ({x}, {y}) is in quadrant two ")
+    else: 
+        print(f"the point ({x}, {y}) is in quadrant three ")
