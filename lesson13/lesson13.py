2e40d5e35405caa64d815d18d28bea5b706bb3ce
Vanessa <vanessaphung07@gmail.com>
2025-02-21 14:27:03 -0500

    cat

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,10 @@
 # Lesson 13
+month = int(input("enter numerical month "))
+day = int(input("enter numerical day "))
+if month == 2 and day == 18:
+    print("special")
+elif month == 2 and day < 18 or month == 1:
+    print("before")
+elif month == 2 and day > 18 or month > 2:
+    print("after")
+    
