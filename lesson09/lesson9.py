84d757d70405471639f75a5f193e29516afcec09
Vanessa <vanessaphung07@gmail.com>
2025-02-21 09:20:41 -0500

    cat

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,29 @@
 # Lesson 9
+import random
+user_input = (input("enter your move: "))
+choices = {1 : 'rock', 2 : 'paper', 3 : 'scissors'}
+ai_move = random.choice(choices)
+if user_input in ('rock', 'paper', 'scissors'):
+    if user_input == ai_move :
+        print("its a tie! ")
+    elif user_input in ('rock'):
+        if ai_move in ('paper'):
+            print("you lose! ")
+           
+        else:
+            print("you win! ")
+            
+    elif user_input in ('paper'):
+        if ai_move in ('scissors'):
+            print("you lose!")
+           
+        else:
+            print("you win!")
+            
+    elif user_input in ('scissors'):
+        if ai_move in ('rock'):
+            print("you lose! ")
+          
+        else:
+            print("you win! ")
+           
