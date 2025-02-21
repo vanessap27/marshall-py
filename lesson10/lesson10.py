12bf7ccdb9e3db63adbd97312fff29b22dc0cb2f
Vanessa <vanessaphung07@gmail.com>
2025-02-21 09:34:24 -0500

    cat

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,8 @@
 # Lesson 10
+phone_num = input("enter the last four digits of the number: ")
+if phone_num[0] in ('8', '9'):
+    if phone_num[1] == phone_num[2]:
+        if phone_num[3] in ('8', '9'):
+            print("the number is a telemarketer ")
+else:
+    print("the number is not a telemarketer ")
