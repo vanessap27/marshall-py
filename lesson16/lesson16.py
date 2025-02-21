69ac8dfe058611d1addb7b59a18bf34d42adbf73
Vanessa <vanessaphung07@gmail.com>
2025-02-21 14:47:31 -0500

    cat

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,11 @@
 # Lesson 16
+num = int(input("enter a number between 1 and 50 "))
+if 1 <= num <= 50:
+    if num % 3 == 0 and num % 5 != 0:
+        print("Fizz")
+    elif num % 5 == 0 and num % 3 != 0:
+        print("Buzz")
+    if  num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(num)
