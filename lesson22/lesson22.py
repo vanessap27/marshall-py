33c7d132b5a75b3f6e8e6bc167d834fa6b3a6a83
Vanessa <vanessaphung07@gmail.com>
2025-02-24 13:17:12 -0500

    cat

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,12 @@
 # Lesson 22
+Nth = int(input("enter a number greater than 0: "))
+fib_0 = 0
+fib_nth = 1
+next_fib = 0
+final_fib = 0
+for term in range(2, Nth):
+    next_fib = fib_0 + fib_nth
+    fib_0 = fib_nth
+    fib_nth = next_fib
+
+print(next_fib)
