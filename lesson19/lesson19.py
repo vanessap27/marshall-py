ac7a4961cf89d6f08f7828b871aa6ddf37aff15f
Vanessa <vanessaphung07@gmail.com>
2025-02-24 11:04:51 -0500

    T-T

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,10 @@
 # Lesson 19
+num =int(input("enter a number "))
+prime = True 
+for factor in range(2, num+1):
+    if num % factor == 0: 
+        print(f"{num} is not a prime number ")
+        prime = False 
+    break
+if prime == True:
+    print(f"{num} is a prime number ")
