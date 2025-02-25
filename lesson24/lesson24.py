120db84167e42fae16f6eccc4e584847de7c66bc
Vanessa <vanessaphung07@gmail.com>
2025-02-25 10:55:04 -0500

    cat

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,13 @@
 # Lesson 24
+condition = True
+longest = ""
+while condition == True:
+    name = input("input a name. input 'X' to stop: ")
+    if name == 'X':
+        break
+    elif len(name) > len(longest):
+        longest = name 
+    elif len(name) == len(longest):
+        longest = longest
+print(f"the longest name is {longest} ")
+
