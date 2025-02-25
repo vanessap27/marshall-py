34ce1fd57fa334df0cf4d62a64054f09e6217bdb
Vanessa <vanessaphung07@gmail.com>
2025-02-25 10:47:29 -0500

    ijehfwoihf

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,16 @@
 # Lesson 23
+condition = True
+sum = 0
+counter = 0
+
+while condition == True:
+    num = input("enter a number to be added to the average ")
+    if num == "X":
+        condition = False 
+    else:
+        sum = sum + int(num)
+        counter += 1
+
+average = sum / counter
+print(f"the average is {average} ")
+
