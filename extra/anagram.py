97c64605efb0415173e3cdc33a7ff39e65e2eec1
Vanessa <vanessaphung07@gmail.com>
2025-03-03 20:55:31 -0500

    cat

---
+++ extra/anagram.py
@@ -0,0 +1,9 @@
+word1 = input("enter word 1: ")
+word2 = input("enter word 2: ")
+reverse = ""
+i = len(word1)
+if len(word1) != len(word2):
+    print("it's not an anagram. ")
+else:
+    while 
+    
