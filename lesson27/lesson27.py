25ff9542a50de717f8ada0cd0c00a54ad118f8c0
Vanessa <vanessaphung07@gmail.com>
2025-03-18 17:31:17 -0400

    cat

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,10 @@
 # Lesson 27
+user_input = input("enter anything: ")
+alpha = []
+if user_input.isalpha():
+    print(user_input)
+else: 
+    for i in user_input:
+        if i.isalpha():
+            alpha.append(i)
+    print(alpha)
