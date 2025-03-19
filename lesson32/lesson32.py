bb66672657d5b094f76e02543a81677997d18049
Vanessa <vanessaphung07@gmail.com>
2025-03-18 22:59:28 -0400

    hi

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,8 @@
 # Lesson 32
+wordA = input("input the first word: ")
+wordB = input('input the second word: ')
+joined = list(wordA + wordB)
+for i in joined:
+    if joined.count(i) > 1:
+        joined.remove(i)
+print(joined)
