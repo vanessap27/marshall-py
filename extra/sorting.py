042ca75c2e6a4ed8556cfa5aa94a75a4bfc16a93
Vanessa <vanessaphung07@gmail.com>
2025-03-19 14:16:07 -0400

    sort

--- extra/sorting.py
+++ extra/sorting.py
@@ -1,10 +1,37 @@
-unsorted = [1, 5, 100, 27, 2, 3, 1]
-sorted = []
+a_list = [1, 5, 100, 27, 2, 3, 1]
 
-while len(unsorted) > 0:    
-    unsorted_min= min(unsorted)
-    sorted.append(unsorted_min)
-    unsorted.remove(unsorted_min)
-   
-print(sorted)
+#1 destructive version (simplest)
+# sorted = []
+# while len(a_list) > 0:  # or while a_list:  
+#     a_list_min= min(a_list)
+#     sorted.append(a_list_min)
+#     a_list.remove(a_list_min)  
+# print(sorted)
 
+
+# 2 without distruction
+
+# for i in range(0, len(a_list)-1):
+#     small = a_list[i]
+#     location = None #havent found a min
+#     for j in range(i+1, len(a_list)):
+#         if a_list[j] < small:
+#             small = a_list[j]
+#             location = j
+#     if location is not None:
+#         a_list[i], a_list[location] = a_list[location], a_list[i]
+# print(a_list)
+
+
+#3 bubble sort
+
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(1, len(a_list)):
+        left = a_list[i - 1]
+        right = a_list[i]
+        if left > right:
+            swapped = True
+            a_list[i-1], a_list[i] = a_list[i], a_list[i-1]
+print(a_list)
