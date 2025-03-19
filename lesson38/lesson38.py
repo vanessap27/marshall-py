7606efc46cebe3f7d14123d609108f25a655334f
Vanessa <vanessaphung07@gmail.com>
2025-03-19 18:19:10 -0400

    cat

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,13 @@
 # Lesson 38
+def palindrome(text):
+    return text == text[::-1]
+
+palindromes = []
+largest = ''
+for num1 in range(999, 99, -1):
+    for num2 in range(999, 99, -1):
+        curr_num = num1 * num2
+        if palindrome(str(curr_num)):
+            palindromes.append(curr_num)
+largest = max(palindromes)
+print(largest)
