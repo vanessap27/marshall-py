9fb7d76da3fe98f31cf727719d8e28a3d99d278c
Vanessa <vanessaphung07@gmail.com>
2025-03-18 21:47:58 -0400

    hi

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,11 @@
 # Lesson 29
+user_input = list(input('enter a phrase/word: '))
+counter = 0
+i = 0
+while i < len(user_input):
+    if user_input[i] == 'a' or user_input[i] == 'e' or user_input[i] == 'i' or user_input[i] == 'o' or user_input[i] == 'u':
+        counter += 0
+    else:
+        counter += 1
+    i += 1
+print(counter)
