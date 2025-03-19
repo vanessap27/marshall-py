802e71e57c2a249acd0ebb5271afe39395f640ac
Vanessa <vanessaphung07@gmail.com>
2025-03-18 21:57:04 -0400

    jewnid

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,12 @@
 # Lesson 30
+num = int(input('enter a random number: '))
+pattern = ''
+i = 1
+
+while i <= num:
+    if i%2 == 0:
+        pattern += '1'
+    else:
+        pattern += '0'
+    i += 1
+print(pattern)
