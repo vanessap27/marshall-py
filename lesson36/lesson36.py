1c0908d6440eb74095c01893d86fb32b9f63ebf5
Vanessa <vanessaphung07@gmail.com>
2025-03-19 16:45:36 -0400

    prime

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,19 @@
 # Lesson 36
+
+
+def factors(num):
+    ''' checks the number of factors of num 
+    '''
+    ctr = 0
+    for i in range(1, num+1):
+        if num % i == 0:    
+            ctr += 1
+            i += 1
+    return ctr
+
+num = int(input("enter a number to find it's prime: "))
+
+if factors(num) == 2:
+    print("prime. ")
+else:
+    print('not prime. ')
