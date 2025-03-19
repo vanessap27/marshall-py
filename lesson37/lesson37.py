feaebe248baf9256411aaa065bfd4c7ab1937750
Vanessa <vanessaphung07@gmail.com>
2025-03-19 17:29:39 -0400

    37

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,24 @@
 # Lesson 37
+
+a_string = input("enter a string: ")
+def repeat(a_string):
+    compressed = ''
+    ctr = 1
+    for i in range(1, len(a_string)):
+        if a_string[i] == a_string[i-1]:
+            ctr += 1
+        else: 
+            compressed += a_string[i-1]
+            compressed += str(ctr)
+           
+            ctr = 1
+
+    return compressed
+
+
+
+    if len(repeat(a_string)) >= len(a_string):
+        return(a_string)
+    else: 
+        return(compressed)
+print(repeat(a_string))
