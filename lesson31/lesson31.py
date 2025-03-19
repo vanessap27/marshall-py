74e21145e36327323325066fc9f380d428e6e2db
Vanessa <vanessaphung07@gmail.com>
2025-03-18 22:34:03 -0400

    feor

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,18 @@
 # Lesson 31
+input_a = list(input("enter your first word: "))
+input_b = list(input("enter your second word: "))
+input_a.sort()
+input_b.sort()
+x = 0
+if input_a == input_b:
+    print("they're anagrams! ")
+elif len(input_a) != len(input_b):
+    print("they're not anagrams")
+else: 
+    for i in input_a:
+        if i == input_b[x]:
+            x += 1
+        else:
+            print("they're not anagrams. ")
+            break 
+        print("they're anagrams! ")
