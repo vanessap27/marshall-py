d472928753c0c8ab8065d9bafdd1cbfb11e957ec
Vanessa <vanessaphung07@gmail.com>
2025-03-18 23:14:42 -0400

    hii

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,11 @@
 # Lesson 33
+num_list = [1, 6, 8, 2, 10, 4]
+num_list.sort()
+median = 0
+i = 0
+sum = 0
+while i < len(num_list):
+    sum += num_list[i]
+    median = sum/len(num_list)
+    i += 1
+print(median)
