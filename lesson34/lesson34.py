8f9081a415a6427efb732210aa05603a4458c4a5
Vanessa <vanessaphung07@gmail.com>
2025-03-18 23:35:46 -0400

    hi

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,12 @@
 # Lesson 34
+# Comma Separated Values 
+# user_input = input("enter a string and separate the values with commas: ")
+# split_input = user_input.split(", ")
+# print(split_input)
+
+# & Generating Random Numbers
+import random
+starts = int(input("what does the range start at: "))
+ends = int(input("what does the range end at: "))
+num = random.randrange(starts, ends)
+print(num)
