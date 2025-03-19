0f56b9c400b8a0005717c789469773582735b900
Vanessa <vanessaphung07@gmail.com>
2025-03-19 13:29:32 -0400

    hi

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,12 @@
 # Lesson 35
+argument = ['a', 'a', 'b', 'c', 'hii', 'cat', 'bat' , 'cat']
+argument.sort()
+single = []
+i = 0
+current_var = argument[i]
+count = argument.count(current_var)
+while i < len(argument):
+    if argument[i] not in single:
+        single.append(argument[i])
+    i += 1
+print(single)
