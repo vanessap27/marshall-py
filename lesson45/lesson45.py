7e65ca8571d956d52d743e0c46d60c939a87e829
Vanessa <vanessaphung07@gmail.com>
2025-03-28 12:47:58 -0400

    jsidsa

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,9 @@
 # Lesson 45
+words = ["apple", "banana", "cherry", "date"]
+def len_dict(a_list):
+    result = {}
+    for word in a_list:
+        if word not in result:
+            result[word] = len(word)
+    return result
+print(len_dict(words))
