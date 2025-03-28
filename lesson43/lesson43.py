dbdb035adc54c544bf447a9e8f75a0b383d105fd
Vanessa <vanessaphung07@gmail.com>
2025-03-28 11:00:46 -0400

    set

--- lesson43/lesson43.py
+++ lesson43/lesson43.py
@@ -1,1 +1,12 @@
 # Lesson 43
+Alist = ['hello', 'world', 'cat', 'dog', 'blueberry', 'hello']
+Blist = ["hello",  'blueberry', 'strawberry', 'cat']
+def duplicates(a_list):
+    result = set(a_list)
+    return(list(result))
+print(duplicates(Alist))
+def intersect(a_list, b_list):
+    x= set(Alist)
+    y =set(Blist)
+    return x.intersection(y)
+print(intersect(Alist, Blist))
